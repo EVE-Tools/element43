bafec269f0f2593d4c0d8bad28b6df5c9872690f
Greg Taylor <gtaylor@l11solutions.com>
2012-07-18 18:31:32 -0400

    Adding a basic OrderMessage model.

--- webapp/element43/apps/market_data/admin.py
+++ webapp/element43/apps/market_data/admin.py
@@ -1,17 +1,28 @@
 from django.contrib import admin
 
-from apps.market_data.models import UUDIFMessage
+from apps.market_data.models import UUDIFMessage, OrderMessage
 
 class UUDIFMessageAdmin(admin.ModelAdmin):
     """
     Admin site definition for the UUDIFMessage model.
     """
 
-    list_display = (
-        'key', 'received_dtime', 'is_order',
-    )
+    list_display = ('key', 'received_dtime', 'is_order')
     list_filter = ('is_order', 'received_dtime')
     search_fields = ('key',)
     date_hierarchy = 'received_dtime'
 
 admin.site.register(UUDIFMessage, UUDIFMessageAdmin)
+
+
+class OrderMessageAdmin(admin.ModelAdmin):
+    """
+    Admin site definition for the OrderMessage model.
+    """
+
+    list_display = ('generated_at', 'is_bid', 'type_id', 'region_id')
+    list_filter = ('is_bid', 'generated_at')
+    search_fields = ('message_key', 'uploader_ip_hash')
+    date_hierarchy = 'generated_at'
+
+admin.site.register(OrderMessage, OrderMessageAdmin)
--- webapp/element43/apps/market_data/models.py
+++ webapp/element43/apps/market_data/models.py
@@ -1,3 +1,7 @@
+"""
+Model definitions for storing market data messages.
+"""
+
 from django.db import models
 
 class UUDIFMessage(models.Model):
@@ -19,3 +23,48 @@ class UUDIFMessage(models.Model):
     class Meta(object):
         verbose_name = "UUDIF Message"
         verbose_name_plural = "UUDIF Messages"
+
+class OrderMessage(models.Model):
+    """
+    A parsed order message with the details broken out into the various fields.
+    This represents a single line in a UUDIF rowset.
+    """
+
+    generated_at = models.DateTimeField(blank=True, null=True,
+        help_text="When the market data was generated on the user's machine.")
+    region_id = models.PositiveIntegerField(
+        help_text="Region ID the order originated from.")
+    type_id = models.BigIntegerField(
+        help_text="The Type ID of the item in the order.")
+    price = models.FloatField(
+        help_text="Item price, as reported in the message.")
+    volume_remaining = models.PositiveIntegerField(
+        help_text="Number of remaining items for sale.")
+    volume_entered = models.PositiveIntegerField(
+        help_text="Number of items initially put up for sale.")
+    minimum_volume = models.PositiveIntegerField(
+        help_text="Minimum volume before the order finishes.")
+    range = models.IntegerField(
+        help_text="This field is stupid.")
+    order_id = models.BigIntegerField(
+        help_text="Unique order ID from EVE for this order.")
+    is_bid = models.BooleanField(
+        help_text="If True, this is a buy order. If False, this is a sell order.")
+    issue_date = models.DateTimeField(
+        help_text="When the order was issued.")
+    duration = models.PositiveSmallIntegerField(
+        help_text="The duration of the order, in seconds.")
+    station_id = models.PositiveIntegerField(
+        help_text="The station that this order is in.")
+    solar_system_id = models.PositiveIntegerField(
+        help_text="ID of the solar system the order is in.")
+    is_suspicious = models.BooleanField(
+        help_text="If this is True, we have reason to question this order's validity")
+    message_key = models.CharField(max_length=255,
+        help_text="The unique hash that of the market message.")
+    uploader_ip_hash = models.CharField(max_length=255,
+        help_text="The unique hash for the person who uploaded this message.")
+
+    class Meta(object):
+        verbose_name = "Order Message"
+        verbose_name_plural = "Order Messages"
