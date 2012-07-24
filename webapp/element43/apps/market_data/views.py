235be972381583f1ebdbaa328f59a8088ff7c60d
Greg Oberfield <gregoberfield@gmail.com>
2012-07-24 19:57:18 +0000

    created some home test files to check skipping HAML

--- webapp/element43/apps/market_data/views.py
+++ webapp/element43/apps/market_data/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render_to_response
 
 def home(request):
     return render_to_response('home.html')
+
+def home-test(request):
+    return render_to_response('home-test.html')
