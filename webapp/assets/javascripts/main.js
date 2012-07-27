f2cdf9c75ecc7a141a55e1180a4d2deb26ea3c89
zweizeichen <info@zweizeichen.org>
2012-07-27 15:24:48 +0200

    Added fancy EVE time clock

--- webapp/assets/javascripts/main.js
+++ webapp/assets/javascripts/main.js
@@ -22,4 +22,38 @@ $(document).ready(function() {
 			});
 		}
 	});
+	
+	// Fancy clock
+	// http://www.alessioatzeni.com/blog/css3-digital-clock-with-jquery/
+
+  // Create a newDate() UTC object
+	var now = new Date();
+	var min = 60*1000;
+	
+  // Output the day, date, month and year   
+  $('#date').html("EVE Time ");
+
+  setInterval(function () {
+			now = new Date();
+      // Create a newDate() object and extract the seconds of the current time on the visitor's
+      var seconds = new Date(now.getTime() + (now.getTimezoneOffset() * min)).getSeconds();
+      // Add a leading zero to seconds value
+      $("#sec").html((seconds < 10 ? "0" : "") + seconds);
+  }, 1000);
+
+  setInterval(function () {
+			now = new Date();
+      // Create a newDate() object and extract the minutes of the current time on the visitor's
+      var minutes = new Date(now.getTime() + (now.getTimezoneOffset() * min)).getMinutes();
+      // Add a leading zero to the minutes value
+      $("#min").html((minutes < 10 ? "0" : "") + minutes);
+  }, 1000);
+
+  setInterval(function () {
+			now = new Date();
+      // Create a newDate() object and extract the hours of the current time on the visitor's
+      var hours = new Date(now.getTime() + (now.getTimezoneOffset() * min)).getHours();
+      // Add a leading zero to the hours value
+      $("#hours").html((hours < 10 ? "0" : "") + hours);
+  }, 1000);
 });
