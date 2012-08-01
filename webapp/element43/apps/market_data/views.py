e36c3df073239196bca83788b6a02cf639d508fa
zweizeichen <info@zweizeichen.org>
2012-08-01 15:29:32 +0200

    Added quick look for regions
    - NumPy is a requirement now
    - Added quick look for regions

--- webapp/element43/apps/market_data/views.py
+++ webapp/element43/apps/market_data/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
 
+# Aggregation
+from django.db.models import Sum
+
 # JSON for the live search
 from django.utils import simplejson
 
@@ -12,6 +15,10 @@ from models import Orders
 
 # eve_db models
 from eve_db.models import InvType
+from eve_db.models import MapRegion
+
+# numpy processing imports
+import numpy as np
 
 """
 Those are our views. We have to use the RequestContext for CSRF protection, 
@@ -92,14 +99,70 @@ def live_search(request, query='a'):
 def quicklook(request, type_id="34"):
 		
 		"""
-		Generated a market overview for a certain type. Default to tritanium.
+		Generates a market overview for a certain type. Default to tritanium.
 		"""
 		
+		# Get requested type
 		type_object = InvType.objects.get(id = type_id)
 		
-		buy_orders = Orders.objects.filter(invtype = type_id, is_bid = True).order_by('-price')[:50]
-		sell_orders = Orders.objects.filter(invtype = type_id, is_bid = False).order_by('price')[:50]
-
-		rcontext = RequestContext(request, {'type':type_object, 'buy_orders':buy_orders, 'sell_orders':sell_orders})
+		# Fetch all buy/sell orders from DB
+		buy_orders = Orders.objects.filter(invtype = type_id, is_bid = True).order_by('-price')
+		sell_orders = Orders.objects.filter(invtype = type_id, is_bid = False).order_by('price')
+		
+		# Make list with all orders
+		orders = []
+		orders += buy_orders
+		orders += sell_orders
+		
+		# Get region IDs of regions with orders for this type
+		regions = []
+		for order in orders:
+			if not order.mapregion_id in regions:
+				regions.append(order.mapregion_id)
+		
+		# Gather region-based data for this type
+		region_data = []
+		for region in regions:
+			# Temporary array for this region - will later be appended to region_data
+			temp_data = []
+		
+			# Get all the prices of this region into a numpy array for processing later on
+			region_ask_prices = np.array([order.price for order in buy_orders if order.mapregion_id == region])
+			region_bid_prices = np.array([order.price for order in sell_orders if order.mapregion_id == region])
+			
+			# Order of array entries: Name, Bid/Ask(Low, High, Average, Median, Standard Deviation, Lots, Volume)
+			temp_data.append(MapRegion.objects.get(id=region).name)
+			
+			if len(region_ask_prices) > 0:
+				# Ask values calculated via numpy
+				temp_data.append(np.min(region_ask_prices))
+				temp_data.append(np.max(region_ask_prices))
+				temp_data.append(np.average(region_ask_prices))
+				temp_data.append(np.median(region_ask_prices))
+				temp_data.append(np.std(region_ask_prices))
+				temp_data.append(len(region_ask_prices))
+				temp_data.append(Orders.objects.filter(mapregion_id = region, invtype = type_id, is_bid = False).aggregate(Sum('volume_remaining'))['volume_remaining__sum'])
+			else:
+				# Else there are no orders in this region -> add a bunch of 0s
+				temp_data.extend([0,0,0,0,0,0,0])
+			
+			if len(region_bid_prices) > 0:
+				# Bid values calculated via numpy
+				temp_data.append(np.min(region_bid_prices))
+				temp_data.append(np.max(region_bid_prices))
+				temp_data.append(np.average(region_bid_prices))
+				temp_data.append(np.median(region_bid_prices))
+				temp_data.append(np.std(region_bid_prices))
+				temp_data.append(len(region_bid_prices))
+				temp_data.append(Orders.objects.filter(mapregion_id = region, invtype = type_id, is_bid = True).aggregate(Sum('volume_remaining'))['volume_remaining__sum'])
+			else:
+				# Else there are no orders in this region -> add a bunch of 0s
+				temp_data.extend([0,0,0,0,0,0,0])
+				
+			# Append temp_data to region_data
+			region_data.append(temp_data)
+		
+		# Use the 50 'best' orders for quicklook and add the region_data to the context
+		rcontext = RequestContext(request, {'type':type_object, 'buy_orders':buy_orders[:50], 'sell_orders':sell_orders[:50], 'regions':region_data})
 		
 		return render_to_response('quicklook.haml', rcontext)
