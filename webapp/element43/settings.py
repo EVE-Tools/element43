15112cf3851ddeac9bedb49b1ae804fb35024e94
zweizeichen <info@zweizeichen.org>
2012-08-27 15:16:50 +0200

    Corrected default e-mail domain

--- webapp/element43/settings.py
+++ webapp/element43/settings.py
@@ -23,7 +23,7 @@ EMAIL_PORT = 25
 EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_TLS = False
-DEFAULT_FROM_EMAIL = 'no_reply@element43.com'
+DEFAULT_FROM_EMAIL = 'no_reply@element-43.com'
 
 MANAGERS = ADMINS
 
