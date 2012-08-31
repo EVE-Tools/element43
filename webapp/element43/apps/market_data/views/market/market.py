7be6c11a3a8d315b5fc4a43dee414d04c84887dc
Greg Oberfield <gregoberfield@gmail.com>
2012-08-31 21:23:10 +0000

    broke orders into 2 tabs buy/sell, also fixed a link error and added new hyperlinks

--- webapp/element43/apps/market_data/views/market/market.py
+++ webapp/element43/apps/market_data/views/market/market.py
@@ -28,6 +28,7 @@ def quicklook_system(request, type_id = 34, region_id = 10000002):
     # Get the item type
     type_object = InvType.objects.get(id = type_id)
     
+# Fetch all buy/sell orders from DB
     buy_orders = Orders.objects.filter(invtype = type_id, is_bid = True, mapregion_id = region_id).order_by('-price')
     sell_orders = Orders.objects.filter(invtype = type_id, is_bid = False, mapregion_id = region_id).order_by('price')
     
@@ -87,74 +88,76 @@ def quicklook_system(request, type_id = 34, region_id = 10000002):
 
 def quicklook(request, type_id = 34):
 		
-		"""
-		Generates a market overview for a certain type. Default to tritanium.
-		"""
-		
-		# Get requested type
-		type_object = InvType.objects.get(id = type_id)
-		
-		# Fetch all buy/sell orders from DB
-		buy_orders = Orders.objects.filter(invtype = type_id, is_bid = True).order_by('-price')
-		sell_orders = Orders.objects.filter(invtype = type_id, is_bid = False).order_by('price')
-		
-		# Make list with all orders
-		orders = []
-		orders += buy_orders
-		orders += sell_orders
-		
-		# Get region IDs of regions with orders for this type
-		regions = []
-		for order in orders:
-			if not order.mapregion_id in regions:
-				regions.append(order.mapregion_id)
-		
-		# Gather region-based data for this type
-		region_data = []
-		for region in regions:
-			# Temporary array for this region - will later be appended to region_data
-			temp_data = []
-		
-			# Get all the prices of this region into a numpy array for processing later on
-			region_ask_prices = np.array([order.price for order in buy_orders if order.mapregion_id == region])
-			region_bid_prices = np.array([order.price for order in sell_orders if order.mapregion_id == region])
-			
-			# Order of array entries: Name, Bid/Ask(Low, High, Average, Median, Standard Deviation, Lots, Volume)
-			temp_data.append(MapRegion.objects.get(id=region).name)
-			
-			if len(region_ask_prices) > 0:
-				# Ask values calculated via numpy
-				temp_data.append(np.min(region_ask_prices))
-				temp_data.append(np.max(region_ask_prices))
-				temp_data.append(round(np.average(region_ask_prices),2))
-				temp_data.append(np.median(region_ask_prices))
-				temp_data.append(round(np.std(region_ask_prices),2))
-				temp_data.append(len(region_ask_prices))
-				temp_data.append(Orders.objects.filter(mapregion_id = region, invtype = type_id, is_bid = False).aggregate(Sum('volume_remaining'))['volume_remaining__sum'])
-                                temp_data.append(region)
-			else:
-				# Else there are no orders in this region -> add a bunch of 0s
-				temp_data.extend([0,0,0,0,0,0,0])
-			
-			if len(region_bid_prices) > 0:
-				# Bid values calculated via numpy
-				temp_data.append(np.min(region_bid_prices))
-				temp_data.append(np.max(region_bid_prices))
-				temp_data.append(round(np.average(region_bid_prices),2))
-				temp_data.append(np.median(region_bid_prices))
-				temp_data.append(round(np.std(region_bid_prices),2))
-				temp_data.append(len(region_bid_prices))
-				temp_data.append(Orders.objects.filter(mapregion_id = region, invtype = type_id, is_bid = True).aggregate(Sum('volume_remaining'))['volume_remaining__sum'])
-                                temp_data.append(region)
-			else:
-				# Else there are no orders in this region -> add a bunch of 0s
-				temp_data.extend([0,0,0,0,0,0,0])
-				
-			# Append temp_data to region_data
-			region_data.append(temp_data)
-		
-		breadcrumbs = group_breadcrumbs(type_object.market_group_id)
-		# Use the 50 'best' orders for quicklook and add the region_data to the context
-		rcontext = RequestContext(request, {'type':type_object, 'buy_orders':buy_orders[:50], 'sell_orders':sell_orders[:50], 'regions':region_data, 'breadcrumbs':breadcrumbs})
-		
-		return render_to_response('market/quicklook.haml', rcontext)
+    """
+    Generates a market overview for a certain type. Default to tritanium.
+    """
+    
+    # Get requested type
+    type_object = InvType.objects.get(id = type_id)
+    
+    # Fetch all buy/sell orders from DB
+    buy_orders = Orders.objects.filter(invtype = type_id, is_bid = True).order_by('-price')
+    sell_orders = Orders.objects.filter(invtype = type_id, is_bid = False).order_by('price')
+    
+    # Make list with all orders
+    orders = []
+    orders += buy_orders
+    orders += sell_orders
+    
+    # Get region IDs of regions with orders for this type
+    regions = []
+    for order in orders:
+        if not order.mapregion_id in regions:
+            regions.append(order.mapregion_id)
+    
+    # Gather region-based data for this type
+    region_data = []
+    for region in regions:
+        # Temporary array for this region - will later be appended to region_data
+        temp_data = []
+    
+        # Get all the prices of this region into a numpy array for processing later on
+        region_ask_prices = np.array([order.price for order in buy_orders if order.mapregion_id == region])
+        region_bid_prices = np.array([order.price for order in sell_orders if order.mapregion_id == region])
+        
+        # Order of array entries: Name, Bid/Ask(Low, High, Average, Median, Standard Deviation, Lots, Volume)
+        temp_data.append(MapRegion.objects.get(id=region).name)
+        
+        if len(region_ask_prices) > 0:
+            # Ask values calculated via numpy
+            temp_data.append(np.min(region_ask_prices))
+            temp_data.append(np.max(region_ask_prices))
+            temp_data.append(round(np.average(region_ask_prices),2))
+            temp_data.append(np.median(region_ask_prices))
+            temp_data.append(round(np.std(region_ask_prices),2))
+            temp_data.append(len(region_ask_prices))
+            temp_data.append(Orders.objects.filter(mapregion_id = region, invtype = type_id, is_bid = False).aggregate(Sum('volume_remaining'))['volume_remaining__sum'])
+            temp_data.append(region)
+        else:
+            # Else there are no orders in this region -> add a bunch of 0s
+            temp_data.extend([0,0,0,0,0,0,0])
+            temp_data.append(region)
+        
+        if len(region_bid_prices) > 0:
+            # Bid values calculated via numpy
+            temp_data.append(np.min(region_bid_prices))
+            temp_data.append(np.max(region_bid_prices))
+            temp_data.append(round(np.average(region_bid_prices),2))
+            temp_data.append(np.median(region_bid_prices))
+            temp_data.append(round(np.std(region_bid_prices),2))
+            temp_data.append(len(region_bid_prices))
+            temp_data.append(Orders.objects.filter(mapregion_id = region, invtype = type_id, is_bid = True).aggregate(Sum('volume_remaining'))['volume_remaining__sum'])
+            temp_data.append(region)
+        else:
+            # Else there are no orders in this region -> add a bunch of 0s
+            temp_data.extend([0,0,0,0,0,0,0])
+            temp_data.append(region)
+            
+        # Append temp_data to region_data
+        region_data.append(temp_data)
+    
+    breadcrumbs = group_breadcrumbs(type_object.market_group_id)
+    # Use the 50 'best' orders for quicklook and add the region_data to the context
+    rcontext = RequestContext(request, {'type':type_object, 'buy_orders':buy_orders[:50], 'sell_orders':sell_orders[:50], 'regions':region_data, 'breadcrumbs':breadcrumbs})
+    
+    return render_to_response('market/quicklook.haml', rcontext)
