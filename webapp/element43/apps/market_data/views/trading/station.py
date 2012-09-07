748accfe7b98fc4acc9b6f33808b12b05c17385d
zweizeichen <info@zweizeichen.org>
2012-09-07 15:44:51 +0200

    Added caching indicator and reduced cache time

--- webapp/element43/apps/market_data/views/trading/station.py
+++ webapp/element43/apps/market_data/views/trading/station.py
@@ -8,15 +8,20 @@ from django.db.models import Count
 from apps.market_data.models import Orders
 from eve_db.models import StaStation
 
+# Time
+import datetime
+
 # Helper functions
 from apps.market_data.util import group_breadcrumbs
 
 def ranking(request, group = 0):
-    
-    """
-    This file generates the station ranks based on active orders in the DB
-    """
-    rank_list = Orders.objects.values('stastation__name').annotate(ordercount=Count('id')).order_by('-ordercount')[:50]
-    
-    rcontext = RequestContext(request, {'rank_list': rank_list})
-    return render_to_response('trading/station/ranking.haml', rcontext)
+	"""
+	This file generates the station ranks based on active orders in the DB
+	"""
+	
+	rank_list = Orders.objects.values('stastation__name').annotate(ordercount=Count('id')).order_by('-ordercount')[:50]
+
+	generated_at = datetime.datetime.now()
+	rcontext = RequestContext(request, {'rank_list': rank_list, 'generated_at': generated_at})
+
+	return render_to_response('trading/station/ranking.haml', rcontext)
