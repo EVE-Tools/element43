cee1de767f534437c3aed7a3cc99850b68a299d9
zweizeichen <info@zweizeichen.org>
2012-09-24 19:38:47 +0200

    Prettified station ranking

--- webapp/element43/apps/market_data/views/trading/station.py
+++ webapp/element43/apps/market_data/views/trading/station.py
@@ -7,6 +7,7 @@ from django.db.models import Count
 from apps.market_data.models import Orders
 from eve_db.models import StaStation
 from eve_db.models import MapRegion
+from eve_db.models import MapSolarSystem
 
 # Time
 import datetime
@@ -18,14 +19,18 @@ from apps.market_data.util import group_breadcrumbs
 from django.views.decorators.cache import cache_page
 
 # Caches this view 1 hour long
-#@cache_page(60 * 60)
+@cache_page(60 * 60)
 def ranking(request, group = 0):
 	"""
 	This file generates the station ranks based on active orders in the DB
 	"""
 	
-	rank_list = Orders.objects.values('stastation__name', 'mapregion__name').annotate(ordercount=Count('id')).order_by('-ordercount')[:50]
-
+	rank_list = Orders.objects.values('stastation__id').annotate(ordercount=Count('id')).order_by('-ordercount')[:50]
+	
+	for rank in rank_list:
+		station = StaStation.objects.get(id = rank['stastation__id'])
+		rank.update({'system': station.solar_system, 'name': station.name, 'region': station.region})
+	
 	generated_at = datetime.datetime.now()
 	rcontext = RequestContext(request, {'rank_list': rank_list, 'generated_at': generated_at})
 
