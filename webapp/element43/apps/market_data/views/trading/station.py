32d2098966f92b464745a244a80378dc0c164f6d
Greg Oberfield <gregoberfield@gmail.com>
2012-09-24 14:03:27 +0000

    Added region name to station rankling list - issue with display though

--- webapp/element43/apps/market_data/views/trading/station.py
+++ webapp/element43/apps/market_data/views/trading/station.py
@@ -6,6 +6,7 @@ from django.db.models import Count
 # eve_db models
 from apps.market_data.models import Orders
 from eve_db.models import StaStation
+from eve_db.models import MapRegion
 
 # Time
 import datetime
@@ -17,13 +18,13 @@ from apps.market_data.util import group_breadcrumbs
 from django.views.decorators.cache import cache_page
 
 # Caches this view 1 hour long
-@cache_page(60 * 60)
+#@cache_page(60 * 60)
 def ranking(request, group = 0):
 	"""
 	This file generates the station ranks based on active orders in the DB
 	"""
 	
-	rank_list = Orders.objects.values('stastation__name').annotate(ordercount=Count('id')).order_by('-ordercount')[:50]
+	rank_list = Orders.objects.values('stastation__name', 'mapregion__name').annotate(ordercount=Count('id')).order_by('-ordercount')[:50]
 
 	generated_at = datetime.datetime.now()
 	rcontext = RequestContext(request, {'rank_list': rank_list, 'generated_at': generated_at})
