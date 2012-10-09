b9e9331c429c458125f7ccff3a874326b60d9411
Jens Lauterbach <lauterjens@googlemail.com>
2012-10-09 23:14:39 +0200

    Bug fix for the calculation when doing multiple runs

--- webapp/assets/javascripts/manufacturing.js
+++ webapp/assets/javascripts/manufacturing.js
@@ -19,53 +19,40 @@ $(document).ready(function () {
     
     //
     // Inline editing of material prices
-    //
-    $('.editable').blur(function(event) {
-      // common vars
+    //    
+    $('#billofmaterials').find('.editable').blur(function(event) {
+      // First step is to get all values and calculate the new values
       var material_id = $(event.target).attr('id').replace("material_", "");
-      
-      // update bill of materials
-      // step 1: update the material price in the corresponding row
+      var blueprint_runs = parseInt($('#blueprint_runs').text().replace(/,/g, ""));
+      var blueprint_cost_unit = parseFloat($('#blueprint_cost_unit').text().replace(/,/g,""));
       var price = $("#material_" + material_id + "").val().replace(/,/g, "");
       var quantity = $("#material_quantity_" + material_id + "").text().replace(/,/g, "");
       var total = parseInt(quantity) * parseFloat(price);
-      //$("#material_price_" + material_id + "").text(addCommas(sprintf("%.2f", total)));
-      $("#material_price_" + material_id + "").text(addCommas(total.toFixed(2)));
-      
-      // step 2: update the total material price per unit
-      var materials_cost_unit = 0;
+      var materials_cost_total = 0;
       
       $('#billofmaterials tbody span[id*="material_price_"]').each(function(index, element) {
           var p = $(this).text().replace(/,/g, '');
-          materials_cost_unit += parseFloat(p);
+          materials_cost_total += parseFloat(p);
       });
       
-      $("#materials_cost_total_unit").text(addCommas(materials_cost_unit.toFixed(2)));
-      
-      // update the cost and profit overview
-      var blueprint_runs = parseInt($('#blueprint_runs').text().replace(/,/g, ""));
-      
-      // step 1: material cost (unit + total)
-      $("#materials_cost_unit").text(addCommas(materials_cost_unit.toFixed(2)));
-      $("#materials_cost_total").text(addCommas((materials_cost_unit * blueprint_runs).toFixed(2)));
-      
-      // step 2: total cost (unit + total)
-      var blueprint_cost_unit = parseFloat($('#blueprint_cost_unit').text().replace(/,/g,""));
-      var total_cost_unit = blueprint_cost_unit + materials_cost_unit;
-      $('#total_cost_unit').text(addCommas(total_cost_unit.toFixed(2)));
-      $('#total_cost_total').text(addCommas((total_cost_unit * blueprint_runs).toFixed(2)));
-      
-      // step 3: profit  (unit + total)
+      var total_cost_unit = blueprint_cost_unit + (materials_cost_total/blueprint_runs);
       var revenue_unit = parseFloat($('#revenue_unit').text().replace(/,/g, ""));
       var profit_unit = revenue_unit - total_cost_unit;
       
+      // Next step is to update all fields
+      $("#material_price_" + material_id + "").text(addCommas(total.toFixed(2)));
+      $("#materials_cost_unit").text(addCommas((materials_cost_total/blueprint_runs).toFixed(2)));
+      $("#materials_cost_total, #materials_cost_total_bom").text(addCommas((materials_cost_total).toFixed(2)));
+      $('#total_cost_unit').text(addCommas(total_cost_unit.toFixed(2)));
+      $('#total_cost_total').text(addCommas((total_cost_unit * blueprint_runs).toFixed(2)));
+      $('#profit_unit').text(addCommas(profit_unit.toFixed(2)));
+      $('#profit_total').text(addCommas((profit_unit * blueprint_runs).toFixed(2)));
+      
+      // Last thing to do is to update css classes
       if (profit_unit > 0) {
         $('#profit_unit, #profit_total').removeClass().addClass("green");
       } else {
         $('#profit_unit, #profit_total').removeClass().addClass("red");  
       }
-      
-      $('#profit_unit').text(addCommas(profit_unit.toFixed(2)));
-      $('#profit_total').text(addCommas((profit_unit * blueprint_runs).toFixed(2)));
     });
 });
