e339c6497b9a718fc90139ac56f167d9e389f0d8
zweizeichen <info@zweizeichen.org>
2012-10-10 18:13:26 +0200

    Removed logging

--- webapp/assets/javascripts/quicklook_graph.js
+++ webapp/assets/javascripts/quicklook_graph.js
@@ -28,7 +28,6 @@ $(document).ready(function () {
                                     }
                                 }
                 counter++;
-                console.log(val);
             });
             
             
