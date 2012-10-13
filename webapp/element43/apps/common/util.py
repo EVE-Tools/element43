a40c52f730c8aa761528a5a6df4f9c4995e225f7
Greg Oberfield <gregoberfield@gmail.com>
2012-10-13 03:25:26 +0000

    added path to system import, also changed param names for pathfinder

--- webapp/element43/apps/common/util.py
+++ webapp/element43/apps/common/util.py
@@ -4,6 +4,17 @@ from django.contrib.auth.models import User
 # API Models
 from apps.api.models import APIKey, Character
 
+# Eve_DB Models
+from eve_db.models import StaStation
+from eve_db.models import MapRegion
+from eve_db.models import MapSolarSystem
+
+# utility functions
+import datetime
+import pytz
+import ast
+import urllib
+
 def validate_characters(user, access_mask):
     """
     Returns characters of a user that match a given minimum access mask.
@@ -20,3 +31,19 @@ def validate_characters(user, access_mask):
             characters = Character.objects.filter(apikey = key)
     
     return characters
+
+def pathfind(start_system, finish_system, seclevel, invert):
+    """
+    Queries the pathfinder daemon for path, returns list of system objects in order
+    """
+    post_params = urllib.urlencode({'start': start_system, 'finish': finish_system, 'seclevel':seclevel, 'invert':invert})
+    path_response = urllib.urlopen('http://localhost:3455/path', post_params)
+    full_path = ast.literal_eval(path_response.read())
+    
+    response = {}
+    path = []
+    for system in full_path:
+        system_object = MapSolarSystem.objects.get(id = system)
+        path.append(system_object)
+    
+    return path
