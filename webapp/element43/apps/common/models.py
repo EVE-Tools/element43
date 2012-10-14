dca36d28fee3d7165e61dabf27c76595d2d42862
zweizeichen <info@zweizeichen.org>
2012-10-14 14:53:25 +0200

    Fixed registration bug

--- webapp/element43/apps/common/models.py
+++ webapp/element43/apps/common/models.py
@@ -38,4 +38,4 @@ def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
-    post_save.connect(create_user_profile, sender=User)
+post_save.connect(create_user_profile, sender=User)
