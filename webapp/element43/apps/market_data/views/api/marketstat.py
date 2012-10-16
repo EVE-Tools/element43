0c70e18d7a1af61a705e1d68c8e3398fddd92f5f
Greg Oberfield <gregoberfield@gmail.com>
2012-10-16 15:21:00 +0000

    refactor market api

--- webapp/element43/apps/market_data/views/api/marketstat.py
+++ webapp/element43/apps/market_data/views/api/marketstat.py
@@ -31,12 +31,12 @@ def legacy_marketstat(request):
         
     stats = ItemRegionStat.objects.get(invtype_id=params['typeid'][0],
                                           mapregion_id=params['regionlimit'][0])
-    buystats = Orders.objects.filter(invtype_id=params['typeid'][0],
+    buystats = Orders.active.filter(invtype_id=params['typeid'][0],
                                      mapregion_id=params['regionlimit'][0],
-                                     is_bid=True, live=True).aggregate(Min('price'), Max('price'), StdDev('price'))
-    sellstats = Orders.objects.filter(invtype_id=params['typeid'][0],
+                                     is_bid=True).aggregate(Min('price'), Max('price'), StdDev('price'))
+    sellstats = Orders.active.filter(invtype_id=params['typeid'][0],
                                      mapregion_id=params['regionlimit'][0],
-                                     is_bid=False, live=True).aggregate(Min('price'), Max('price'), StdDev('price'))
+                                     is_bid=False).aggregate(Min('price'), Max('price'), StdDev('price'))
     
     rcontext = RequestContext(request, {'params':params,
                                         'stats':stats,
