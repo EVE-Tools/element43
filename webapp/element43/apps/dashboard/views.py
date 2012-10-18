2dc445ab6c5e5ab2ba8c2f8041763bfb684abd97
zweizeichen <info@zweizeichen.org>
2012-10-18 20:11:51 +0200

    Added basic order overview to dashboard

--- webapp/element43/apps/dashboard/views.py
+++ webapp/element43/apps/dashboard/views.py
@@ -10,21 +10,50 @@ from django.core.urlresolvers import reverse
 from django.db.models import Sum
 
 # API Models
-from apps.api.models import Character, APITimer, CharSkill
+from apps.api.models import Character, APITimer, CharSkill, MarketOrder
 from apps.common.util import validate_characters
 
 
 @login_required
 def dashboard(request):
+    """
+    Shows basic information about you account so you can quickly get an overview.
+    """
 
-    chars_market = validate_characters(request.user, 8)
-    market_data = []
+    # Sheet based data
+    chars_sheet = validate_characters(request.user, 8)
+    sheet_data = []
 
-    for char in chars_market:
-        market_data.append({'char': char, 'next_update': APITimer.objects.get(character_id=char.id,
+    for char in chars_sheet:
+        sheet_data.append({'char': char, 'next_update': APITimer.objects.get(character_id=char.id,
                                                                               apisheet='CharacterSheet').nextupdate})
 
-    rcontext = RequestContext(request, {'market_data': market_data})
+    # Order based data
+    chars_order = validate_characters(request.user, 4096)
+    market_data = {}
+
+    market_data['ask'] = []
+    market_data['ask_volume'] = 0
+
+    market_data['bid'] = []
+    market_data['bid_volume'] = 0
+
+    market_data['total_volume'] = 0
+
+    # Calculate volumes
+    for char in chars_order:
+        market_data['ask'] += MarketOrder.objects.filter(character=char, order_state=0, id__is_bid=False)
+        market_data['bid'] += MarketOrder.objects.filter(character=char, order_state=0, id__is_bid=True)
+
+    for order in market_data['ask']:
+        market_data['ask_volume'] += -1 * order.id.price * order.id.volume_remaining
+
+    for order in market_data['bid']:
+        market_data['ask_volume'] += order.id.price * order.id.volume_remaining
+
+    market_data['total_volume'] = market_data['ask_volume'] + market_data['bid_volume']
+
+    rcontext = RequestContext(request, {'sheet_data': sheet_data, 'market_data': market_data})
     return render_to_response('dashboard/dashboard.haml', rcontext)
 
 
