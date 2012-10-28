9ed60134c8a3a126480dd376d3005c1aa7f4c6ff
zweizeichen <info@zweizeichen.org>
2012-10-28 20:25:46 +0100

    Added journal view

--- webapp/element43/apps/wallet/views.py
+++ webapp/element43/apps/wallet/views.py
@@ -62,37 +62,23 @@ def transactions(request):
     rcontext = RequestContext(request, {'transactions': transactions})
     return render_to_response('wallet/transactions.haml', rcontext)
 
+
 def journal(request):
 
     # Get all characters with sufficient permissions
     chars = validate_characters(request.user, calculate_character_access_mask(['WalletJournal']))
 
     # Prepare lists
-    orders_bid = []
-    orders_ask = []
-    transactions = {}
     journal = {}
 
     # Get the data
     for char in chars:
-        orders_bid += MarketOrder.objects.filter(character=char,
-                                                 id__is_bid=True).select_related('id').extra(select={'total_value': "price * volume_entered",
-                                                                                                     'total_value_remaining': "price * volume_remaining"}).order_by('order_state')
-
-        orders_ask += MarketOrder.objects.filter(character=char,
-                                                 id__is_bid=False).select_related('id').extra(select={'total_value': "price * volume_entered",
-                                                                                                      'total_value_remaining': "price * volume_remaining"}).order_by('order_state')
-
-        transactions[char.id] = MarketTransaction.objects.filter(character=char).select_related('ref_type')
         journal[char.id] = JournalEntry.objects.filter(character=char)
 
     # Add data to context
-    rcontext = RequestContext(request, {'orders_bid': orders_bid,
-                                        'orders_ask': orders_ask,
-                                        'transactions': transactions,
-                                        'journal': journal})
+    rcontext = RequestContext(request, {'journal': journal})
 
-    return render_to_response('wallet/wallet.haml', rcontext)
+    return render_to_response('wallet/journal.haml', rcontext)
 
 def wallet(request):
 
