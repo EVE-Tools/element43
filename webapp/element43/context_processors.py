6e6b5fd822e4777b775a5c0c271f792a035744a4
zweizeichen <info@zweizeichen.org>
2012-10-29 17:05:50 +0100

    Small workaround for custom context

--- webapp/element43/context_processors.py
+++ webapp/element43/context_processors.py
@@ -8,9 +8,12 @@ def element43_settings(request):
     """
 
     # Wallet / Journal chars
-
-    if request.user.is_authenticated:
-        journal_chars = validate_characters(request.user, calculate_character_access_mask(['WalletJournal']))
-        transaction_chars = validate_characters(request.user, calculate_character_access_mask(['WalletTransactions']))
+    # Check if user is authenticated, if not - just use context without characters
+    try:
+        if request.user.is_authenticated:
+            journal_chars = validate_characters(request.user, calculate_character_access_mask(['WalletJournal']))
+            transaction_chars = validate_characters(request.user, calculate_character_access_mask(['WalletTransactions']))
+    except TypeError:
+        return {'IMAGE_SERVER': settings.IMAGE_SERVER}
 
     return {'IMAGE_SERVER': settings.IMAGE_SERVER, 'characters_journal': journal_chars, 'characters_transaction': transaction_chars}
