3325ee6116642e61446e242e3083a2d2e29eccc3
Greg Oberfield <gregoberfield@gmail.com>
2012-11-05 16:10:08 +0000

    legacy API now handles multiple items per request

--- webapp/element43/apps/market_data/views/api/marketstat.py
+++ webapp/element43/apps/market_data/views/api/marketstat.py
@@ -25,22 +25,31 @@ def legacy_marketstat(request):
     """
     
     params = {}
+    mapregion = 10000002
+    result_info = []
     # parse GET parameters and put them into a dict to make life easier
     for key in request.GET.iterkeys():
         params[key]=request.GET.getlist(key)
+    
+    try:
+        mapregion = int(params['regionlimit'][0])
+    except:
+        mapregion = 10000002
         
-    stats = ItemRegionStat.objects.get(invtype_id=params['typeid'][0],
-                                          mapregion_id=params['regionlimit'][0])
-    buystats = Orders.active.filter(invtype_id=params['typeid'][0],
-                                     mapregion_id=params['regionlimit'][0],
-                                     is_bid=True).aggregate(Min('price'), Max('price'), StdDev('price'))
-    sellstats = Orders.active.filter(invtype_id=params['typeid'][0],
-                                     mapregion_id=params['regionlimit'][0],
-                                     is_bid=False).aggregate(Min('price'), Max('price'), StdDev('price'))
+    for item in params['typeid']:
+        stats = ItemRegionStat.objects.get(invtype_id=item,
+                                              mapregion_id=mapregion)
+        buystats = Orders.active.filter(invtype_id=item,
+                                         mapregion_id=mapregion,
+                                         is_bid=True).aggregate(Min('price'), Max('price'))
+        sellstats = Orders.active.filter(invtype_id=item,
+                                         mapregion_id=mapregion,
+                                         is_bid=False).aggregate(Min('price'), Max('price'))
+        result_info.append({'invtype':item, 'stats':stats, 'buystats':buystats, 'sellstats':sellstats})
+        
+        
     
     rcontext = RequestContext(request, {'params':params,
-                                        'stats':stats,
-                                        'buystats':buystats,
-                                        'sellstats':sellstats})
+                                        'result_info':result_info})
         
     return render_to_response('market/api/legacy_marketstat.haml', rcontext, mimetype="application/xhtml+xml")
