cd0e75bcbe05351015134a15c9022a7b78cbf657
Jens Lauterbach <lauterjens@googlemail.com>
2013-02-16 17:41:53 +0100

    Refactored a lot of code concerning the calculation of the bill of materials. The bill of material is now calculated by a single function, not two like before. There is no differentiation between Tech I and 'Advanced' manufacturing anymore.

--- webapp/element43/apps/manufacturing/functions.py
+++ webapp/element43/apps/manufacturing/functions.py
@@ -1,4 +1,5 @@
 import math
+import itertools
 
 from decimal import Decimal
 
@@ -25,14 +26,6 @@ def is_tech1(type_id):
     return InvBlueprintType.objects.filter(product_type__id=type_id, tech_level=1).exists()
 
 
-def is_advanced_manufacturing(blueprint):
-    """
-    Returns 'True' if the given blueprints requires more advanced production calculations.
-    Normally those are all blueprints for items > Tech I.
-    """
-    return RamTypeRequirement.objects.filter(type=blueprint, type__is_published=True).exists()
-
-
 def calculate_quantities(form_data, blueprint, materials):
     """
     Returns the the given materials dictionary with the calculated quantities for all items.
@@ -51,8 +44,8 @@ def calculate_quantities(form_data, blueprint, materials):
 
 def calculate_quantity(form_data, blueprint, material):
     """
-    Returns the the given material with the calculated quantities for this material.
-    The quantity of a material depends on:
+    Returns the the given material with the calculated quantities for this
+    material. The quantity of a material depends on:
 
     - Blueprint base waste
     - Blueprint material level (ME)
@@ -91,136 +84,153 @@ def calculate_material_prices(materials):
     try:
         # Build the list of material ids for which the price has to be fetched
         material_ids = [material['id'] for material in materials]
-        materials_prices = ItemRegionStat.objects.values('invtype__id', 'sell_95_percentile').filter(invtype_id__in=material_ids,
-                                                                                             mapregion_id__exact=10000002)
+        materials_prices = ItemRegionStat.objects.values(
+            'invtype__id',
+            'sell_95_percentile'
+        ).filter(invtype_id__in=material_ids, mapregion_id__exact=10000002)
 
         for material_price in materials_prices:
             for material in materials:
                 if material['id'] == material_price['invtype__id']:
                     material['price'] = material_price['sell_95_percentile']
                     material['price_total'] = material_price['sell_95_percentile'] * material['quantity']
-
     except Exception:
         connection._rollback()
 
     return materials
 
-def get_materials(form_data, blueprint):
+
+def get_ramtyperequirements_materials(blueprint):
     """
-    Returns the amount of materials required for manufacturing the given blueprint.
+    Returns all the RamTypeRequirements for the given blueprint that are not a
+    skill and required for manufacturing (activity).
     """
-    if is_advanced_manufacturing(blueprint):
-        materials = get_tech2_materials(form_data, blueprint)
-    else:
-        materials = get_tech1_materials(form_data, blueprint)
+    materials = RamTypeRequirement.objects.values(
+        'required_type__id',
+        'required_type__name',
+        'required_type__volume',
+        'quantity',
+        'recycle'
+    ).filter(
+        type__id=blueprint.blueprint_type.id,
+        activity_type__id=1                             # manufacturing = 1
+    ).exclude(required_type__group__category__id=16)    # skill books = 16
     
     return materials
 
-def get_tech1_materials(form_data, blueprint):
+
+def get_invtypematerials(blueprint):
     """
-    Returns the bill of materials for a Tech I blueprint.
-    The bill of materials already contains prices but they are not calculated yet.
-    If you want to get prices for the materials in the bill of materials you have
-    to pass the result of this function to 'calculate_material_prices'!
+    Returns the InvTypeMaterials for the given blueprint.
+    """
+    materials = InvTypeMaterial.objects.values(
+        'material_type__id',
+        'material_type__name',
+        'material_type__volume',
+        'quantity'
+    ).filter(type=blueprint.product_type)
+    
+    return materials
+
+
+def is_required_tech1_item(build_requirement):
+    """
+    Determines if the given build requirement is a tech 1 item that is
+    recyclable.
+    """
+    is_recycle = build_requirement['recycle'] == True
+    is_tech1_item = is_tech1(build_requirement['required_type__id'])
+    
+    return is_recycle and is_tech1_item
+
+
+def get_tech1_item_materials(build_requirements):
+    """
+    Returns the materials needed for the Tech I item that is required for a
+    Tech II product. If the given build requirement does not belong to a Tech II
+    item an empty list will be returned.
     """
     materials = []
-    base_materials = InvTypeMaterial.objects.values('material_type__id',
-                                                    'material_type__name',
-                                                    'material_type__volume',
-                                                    'quantity').filter(type=blueprint.product_type)
-
-    for base_material in base_materials:
-        materials.append(dict({
-            'id': base_material['material_type__id'],
-            'name': base_material['material_type__name'],
-            'quantity': base_material['quantity'],
-            'volume': base_material['material_type__volume'],
-            'price': 0,
-            'price_total': 0,
-            'producible': is_producible(base_material['material_type__id'])
-        }))
-
-    materials = calculate_quantities(form_data, blueprint, materials)
+    
+    for build_requirement in build_requirements:
+        if is_required_tech1_item(build_requirement):
+            materials = InvTypeMaterial.objects.values(
+                'material_type__id',
+                'material_type__name',
+                'material_type__volume',
+                'quantity'
+            ).filter(type=build_requirement['required_type__id'])
+            break
 
     return materials
 
 
-def get_tech2_materials(form_data, blueprint):
+def merge_bill_of_materials(materials1, materials2):
     """
-    Returns the bill of materials for a Tech II blueprint.
-    The bill of materials already contains prices but they are not calculated yet.
-    If you want to get prices for the materials in the bill of materials you have
-    to pass the result of this function to 'calculate_material_prices'!
+    Returns a merged bill of materials from the two given bill of materials.
     """
+    # @TODO: This is so uber ugly that I don't know what to say. But it works.
+    # If you are reading this and know how to make it look/work better please
+    # feel free.
+    materials = []
+    
+    for item in itertools.chain(materials1, materials2):
+        is_in_materials = False
+    
+        for material in materials:
+            if material['id'] == item['id']:
+                is_in_materials = True
+                break
+    
+        if is_in_materials:
+            for material in materials:
+                if material['id'] == item['id']:
+                    material['quantity'] += item['quantity']
+                    break
+        else:
+            materials.append(item)
+    
+    return materials
 
-    materials = []  # bill of materials
 
+def get_materials(form_data, blueprint):
     """
-    Calculating the bill of materials for a Tech II blueprint is a bit tricky. Most
-    Tech II blueprints require a Tech I item and additional materials to manufacture
-    the Tech II item.
-
-    Target Painter II for example requires Target Painter I. One of the exceptions
-    to that rule are Tech II rigs. They don't require their Tech I variant.
-
-    To calculate the bill of materials three things have to be considered:
-
-    1. Materials needed for the Tech I item (tech1_item_materials)
-    2. Materials needed for the Tech II item (tech2_item_materials)
-    3. Build requirements for the Tech II item (build_requirements)
-
-    materials = (tech2_item_materials - tech1_item_materials) + build_requirements
-
-    The bill of materials for the Tech II includes the materials required to manufacture
-    the Tech I item. Therefor the bill of materials for Tech I item have to be
-    subtracted to get a list of materials that are exclusively needed for the
-    Tech II item.
+    Returns the bill of material for the given blueprint.
     """
-
+    materials1 = []
+    materials2 = []
+    
     blueprint_runs = int(form_data['blueprint_runs'])
-    tech1_item_materials = []
-
-    # Find build requirements for the Tech II item (does contain the Tech I item if required)
-    build_requirements = RamTypeRequirement.objects.values('required_type__id',
-                                                           'required_type__name',
-                                                           'required_type__volume',
-                                                           'quantity', 'recycle').filter(type__id=blueprint.blueprint_type.id,
-                                                                                         activity_type__id=1).exclude(required_type__group__category__id=16)
-
+    
+    build_requirements = get_ramtyperequirements_materials(blueprint)
+    tech1_item_materials = get_tech1_item_materials(build_requirements)
+    
     for build_requirement in build_requirements:
-        materials.append(dict({
+        type_volume = build_requirement['required_type__volume']
+        quantity = build_requirement['quantity']
+        
+        materials1.append(dict({
             'id': build_requirement['required_type__id'],
             'name': build_requirement['required_type__name'],
             'quantity': build_requirement['quantity'] * blueprint_runs,
-            'volume': build_requirement['required_type__volume'] * build_requirement['quantity'] * blueprint_runs,
+            'volume': type_volume * quantity * blueprint_runs,
             'price': 0,
             'price_total': 0,
             'producible':is_producible(build_requirement['required_type__id'])
         }))
-
-        # If recycle is True the build_requirement is the required Tech I item.
-        if build_requirement['recycle'] == True and is_tech1(build_requirement['required_type__id']):
-
-            tech1_item_materials = InvTypeMaterial.objects.values('material_type__id',
-                                                                  'material_type__name',
-                                                                  'material_type__volume',
-                                                                  'quantity').filter(type=build_requirement['required_type__id'])
-
+    
     # Get the bill of materials for the Tech II item
-    extra_materials = InvTypeMaterial.objects.values('material_type__id',
-                                                     'material_type__name',
-                                                     'material_type__volume',
-                                                     'quantity').filter(type=blueprint.product_type)
-
+    extra_materials = get_invtypematerials(blueprint)
+    
     for extra_material in extra_materials:
-        # If on of the materials from the bill of materials of the Tech II item is
-        # found in the bill of materials for the Tech I item substract them.
+        # If on of the materials from the bill of materials of the Tech II item
+        # is found in the bill of materials for the Tech I item substract them.
         for tech1_item_material in tech1_item_materials:
             if tech1_item_material['material_type__id'] == extra_material['material_type__id']:
                 extra_material['quantity'] -= tech1_item_material['quantity']
-
-        # Only if the quantity of the material is greater 0 after the substraction add the
-        # material to the bill of materials.
+    
+        # Only if the quantity of the material is greater 0 after the
+        # substraction add the material to the bill of materials.
         if extra_material['quantity'] > 0:
             mat = {
                 'id': extra_material['material_type__id'],
@@ -231,11 +241,14 @@ def get_tech2_materials(form_data, blueprint):
                 'price_total': 0,
                 'producible':is_producible(extra_material['material_type__id'])
             }
-
+    
             mat = calculate_quantity(form_data, blueprint, mat)
-            materials.append(mat)
-
+            materials2.append(mat)
+    
+    materials = merge_bill_of_materials(materials1, materials2)
+    
     return materials
+
 
 def calculate_production_time(form_data, blueprint):
     """ Returns the production time for the given blueprint. """
@@ -250,10 +263,10 @@ def calculate_production_time(form_data, blueprint):
     """
     # implant modifiers. (type_id, modifier)
     IMPLANT_MODIFIER = {
-        0: 0.00,  # no hardwiring
+        0: 0.00,      # no hardwiring
         27170: 0.01,  # Zainou 'Beancounter' Industry BX-801
         27167: 0.02,  # Zainou 'Beancounter' Industry BX-802
-        27171: 0.04  # Zainou 'Beancounter' Industry BX-804
+        27171: 0.04   # Zainou 'Beancounter' Industry BX-804
     }
     
     # calculate production time modifuer
