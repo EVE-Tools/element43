4816d93561558dd7cbdd3487df22224d36b5a84a
zweizeichen <info@zweizeichen.org>
2013-03-02 17:44:30 +0100

    Reworked compression and static file handling

---
+++ webapp/prepare_static.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# This script automates the compression of element43's assets.
+# This has to be done every time a new build is deployed
+#
+
+cd "$(dirname "$0")"
+
+echo 'Collecting static files...'
+django-admin.py collectstatic --noinput --clear
+
+echo 'Converting HAML files to HTML...'
+find . -name '*.haml' -print0 | xargs -0 -I% hamlpy % %.html
+
+echo 'Compressing (S)CSS and JS...'
+django-admin.py compress
+
+echo 'Removing HTML files...'
+find . -name "*.html" -print0 | xargs -0 rm
+
+echo 'Done.'
