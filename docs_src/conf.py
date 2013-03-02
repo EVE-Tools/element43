3182c75af15776cab0032e70521453a79bbc9228
zweizeichen <info@zweizeichen.org>
2013-03-02 21:22:36 +0100

    Yet another sphinx fix

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -310,6 +310,6 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['pylibmc', 'djcelery', 'ujson', 'pytz', 'eve_db.models']
+MOCK_MODULES = ['pylibmc', 'djcelery', 'ujson', 'pytz', 'eve_db', 'models']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
