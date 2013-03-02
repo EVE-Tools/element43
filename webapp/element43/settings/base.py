99d4fa351c6ecb11a8d4b918c2cde513120d5bbc
zweizeichen <info@zweizeichen.org>
2013-03-02 15:19:00 +0100

    Removed old assets directory

--- webapp/element43/settings/base.py
+++ webapp/element43/settings/base.py
@@ -159,7 +159,6 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    PROJECT_ROOT.child('assets'),
 )
 
 # List of finder classes that know how to find static files in
