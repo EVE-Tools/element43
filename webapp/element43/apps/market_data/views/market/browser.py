9d6d8858c8b549540016171b7074fc19097079a3
zweizeichen <info@zweizeichen.org>
2013-04-01 17:38:45 +0200

    Removed prices from market browser in favor of better loading times

--- webapp/element43/apps/market_data/views/market/browser.py
+++ webapp/element43/apps/market_data/views/market/browser.py
@@ -6,37 +6,16 @@ from django.template import RequestContext
 from eve_db.models import InvType
 from eve_db.models import InvMarketGroup
 
-# Models
-from apps.market_data.models import Orders
-
 
 def panel(request, group=0):
     """
     Render panel.
     """
-    # Check if group is defined
-    prices = {}
-
-    types = InvType.objects.filter(market_group=group, is_published=True)
-
-    for item in types:
-        try:
-            ask = Orders.active.filter(invtype=item, is_bid=False).order_by("price")[:1][0].price
-        except:
-            ask = 0
-
-        try:
-            bid = Orders.active.filter(invtype=item, is_bid=True).order_by("-price")[:1][0].price
-        except:
-            bid = 0
-
-        prices.update({item.id: {'ask': ask, 'bid': bid}})
 
     # If there are types in this group render type template
     rcontext = RequestContext(request, {'parent_name': InvMarketGroup.objects.get(id=group).name,
                                         'types': InvType.objects.filter(market_group=group,
-                                        is_published=True),
-                                        'prices': prices})
+                                        is_published=True)})
 
     return render_to_response('browser/types.haml', rcontext)
 
