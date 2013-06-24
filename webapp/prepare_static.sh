35f0ec8bfcd81c22648448eeb97ae1d205d350b6
zweizeichen <info@zweizeichen.org>
2013-06-24 10:08:40 +0200

    Added full set of type icons (including x16 versions)  to project. Odyssey asset update part II.

--- webapp/prepare_static.sh
+++ webapp/prepare_static.sh
@@ -4,11 +4,18 @@
 # This has to be done every time a new build is deployed
 #
 
-# Example for resizing icons from the data dump using imagemagick:
+# Example for resizing icons from the data dump using imagemagick (in Icons directory):
 #
+# 
+# mkdir resize
 # for file in items/*; do convert $file -resize 16x16 resize/`basename $file`; done
 # cd resize
 # for i in `ls -1`; do mv "$i" "`echo $i | awk '{sub("_16_","_")}1' | awk '{sub("_32_","_")}1' | awk '{sub("_64_","_")}1' | awk '{sub("_128_","_")}1'`"; done
+#
+
+# To Resize Types (in Type directory)
+#
+# for i in `ls -1 | grep "_32"`; do convert $i -resize 16x16 `echo $i | awk '{sub("_32","_16")}1'`; done
 #
 
 cd "$(dirname "$0")"
