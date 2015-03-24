669cb8ece516d68bf455bd1ae1605338c5fc5be1
Sebastian <info@zweizeichen.org>
2015-03-24 12:37:27 +0100

    Requirements update

--- webapp/element43/settings/base.py
+++ webapp/element43/settings/base.py
@@ -110,8 +110,8 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.BrowsableAPIRenderer',
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.YAMLRenderer',
-        'rest_framework.renderers.XMLRenderer',
+        'rest_framework_yaml.renderers.YAMLRenderer',
+        'rest_framework_xml.renderers.XMLRenderer',
     ),
 
     'DEFAULT_THROTTLE_CLASSES': (
